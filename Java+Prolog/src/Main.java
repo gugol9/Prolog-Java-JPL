6f3da18b5fc98b4fea1fe51e3d4b78a28fddb406
Kamil Markowski <markian99@wp.pl>
2023-01-09 19:51:02 +0100

    1.2

--- Java+Prolog/src/Main.java
+++ Java+Prolog/src/Main.java
@@ -1,23 +1,29 @@
 import java.util.Map;
+import java.util.Scanner;
+
 import org.jpl7.*;
 
 public class Main {
     public static void main(String argv[]) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wybierz kategorie chrób");
+        System.out.println("Wpisz '1' jeśli chcesz zobaczyć jakie są choroby.");
+        System.out.println("Wpisz '2' jeśli chcesz zobaczyć jakie są nowotwory.");
+        System.out.println("Wpisz '3' jeśli chcesz zobaczyć diagnozę danej choroby.");
+        System.out.println("Wpisz '4' jeśli chcesz zobaczyć diagnozę danego nowotworu.");
         String number = scanner.nextLine();
 
-        switch (number){
-            case "1":
-                System.out.println("Wyświetl choroby");
+
+        switch (number) {
+            case "1" -> {
+
                 try {
                     System.out.println("Choroby");
                     Query query = new Query("consult('src/med.pl')");
                     query.hasSolution();
                     query = new Query("choroby(X)");
 
-                    Map < String, Term > [] res = query.allSolutions();
+                    Map<String, Term>[] res = query.allSolutions();
                     for (int i = 0; i < res.length; i++) {
                         System.out.println(String.valueOf(res[i]));
                     }
@@ -25,17 +31,36 @@ public class Main {
                     System.err.println("Native code library failed to load.\n" + e);
                     System.exit(1);
                 }
-                break;
+            }
+            case "2" -> {
 
-            case "2":
-                System.out.println("DIAGNOZA");
                 try {
-                    System.out.println("Obrajwy tej choroby");
+                    System.out.println("Nowotwory");
+                    Query query = new Query("consult('src/med.pl')");
+                    query.hasSolution();
+                    query = new Query("nowotwor(X)");
+
+                    Map<String, Term>[] res = query.allSolutions();
+                    for (int i = 0; i < res.length; i++) {
+                        System.out.println(String.valueOf(res[i]));
+                    }
+                } catch (UnsatisfiedLinkError e) {
+                    System.err.println("Native code library failed to load.\n" + e);
+                    System.exit(1);
+                }
+
+            }
+            case "3" -> {
+                System.out.println("DIAGNOZA");
+                System.out.println("Pamiętaj o kropce po chorobie.");
+
+                try {
+                    System.out.println("Wprowadz nazwę choroby a zobaczysz objawy.");
                     Query query = new Query("consult('src/med.pl')");
                     query.hasSolution();
                     query = new Query("diagnoza(_,A,B,C,D)");
-
-                    Map < String, Term > [] res = query.allSolutions();
+// po nazwie choroby dawać kropkę.
+                    Map<String, Term>[] res = query.allSolutions();
                     for (int i = 0; i < res.length; i++) {
                         System.out.println(String.valueOf(res[i]));
                     }
@@ -43,8 +68,27 @@ public class Main {
                     System.err.println("Native code library failed to load.\n" + e);
                     System.exit(1);
                 }
-                break;
+            }
 
+            case "4" -> {
+                System.out.println("Nowotwory");
+                System.out.println("Pamiętaj o kropce po nowotworze.");
+
+                try {
+                    System.out.println("Wprowadz nazwę nowotworu a zobaczysz objawy.");
+                    Query query = new Query("consult('src/med.pl')");
+                    query.hasSolution();
+                    query = new Query("diagnoza_nowotwor(_,A,B,C,D)");
+// po nazwie choroby dawać kropkę.
+                    Map<String, Term>[] res = query.allSolutions();
+                    for (int i = 0; i < res.length; i++) {
+                        System.out.println(String.valueOf(res[i]));
+                    }
+                } catch (UnsatisfiedLinkError e) {
+                    System.err.println("Native code library failed to load.\n" + e);
+                    System.exit(1);
+                }
+            }
         }
 
 
--- Java+Prolog/src/med.pl
+++ Java+Prolog/src/med.pl
@@ -7,6 +7,14 @@ choroby(depresja).
 choroby(schizofrenia).
 choroby(anoreksja).
 
+nowotwor(czerniak).
+nowotwor(trzustki).
+nowotwor(watroby).
+nowotwor(zoladka).
+nowotwor(jelita_grubego).
+
+
+
 choroba(achadlazja,zaburzenia_polykania,zgaga,przewlekly_kaszel, krztuszenie_sie).
 choroba(glejak_nerwu_wzrokowego,wytrzeszcz_galki_ocznej,zgaga,zgaga,uposledzenie_widzenia).
 choroba(ebola, bola_glowy, bol_miesni, bol_stawow, krwawa_biegunka).
@@ -15,7 +23,16 @@ choroba(swiatlowstret,zapalenie_spojowek, zapalenie_wnetrza_galki_ocznej, beztec
 choroba(depresja, zaburzenia_snu, spadek_libido, zmniejszenie_lub_utrata_apetytu, dobowe_wahanie_samopoczucia).
 choroba(schizofrenia, halucynacje, urojenia_ksobne, anhedonia, awolicja).
 choroba(anoreksja, obnizenie_nastroju, poczucie_braku_sensu_zycia, mysli_samobojcze, objawy_myslenia_obsesyjnego).
+nowotwory(czerniak, ksztalt_asymetryczny, nierowne_brzegi, swedzenie, wielkosc_wieksze_niz_6_mm).
+nowotwory(trzustki, zoltaczka, swiad_skory, spadek_masy_ciala, zapalenie_zyl).
+nowotwory(watroby, bol_brzucha, obrzeki_konczyn_dolnych, wzdecie_brzucha, zoltaczka).
+nowotwory(zoladka, chudniecie, brak_apetytu, uciazliwe_odbijanie_po_posilku, powiekszeniem_obwodu_brzucha).
+nowotwory(jelita_grubego, uporczywe_biegunki, bol_i_skurcze_brzucha, anemia, zaparcia).
 
 diagnoza(X,A,B,C,D):-
             read(X),nl,
     choroba(X,A,B,C,D).
+
+diagnoza_nowotwor(X,A,B,C,D):-
+            read(X),nl,
+    nowotwory(X,A,B,C,D).
