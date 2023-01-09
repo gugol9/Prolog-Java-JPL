654cdf691113a63bb76e09479b93edd5f8ebd54f
Kamil Markowski <markian99@wp.pl>
2023-01-09 18:27:21 +0100

    new disease

--- Java+Prolog/src/Main.java
+++ Java+Prolog/src/Main.java
@@ -3,33 +3,52 @@ import org.jpl7.*;
 
 public class Main {
     public static void main(String argv[]) {
-        try {
-            System.out.println("Choroby");
-            Query query = new Query("consult('src/med.pl')");
-            query.hasSolution();
-            query = new Query("choroby(X)");
 
-            Map < String, Term > [] res = query.allSolutions();
-            for (int i = 0; i < res.length; i++) {
-                System.out.println(String.valueOf(res[i]));
-            }
-        } catch (UnsatisfiedLinkError e) {
-            System.err.println("Native code library failed to load.\n" + e);
-            System.exit(1);
-        }
-        try {
-            System.out.println("Obrajwy tej choroby");
-            Query query = new Query("consult('src/med.pl')");
-            query.hasSolution();
-            query = new Query("diagnoza(_,A,B,C,D)");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wybierz kategorie chrób");
+        String number = scanner.nextLine();
 
-            Map < String, Term > [] res = query.allSolutions();
-            for (int i = 0; i < res.length; i++) {
-                System.out.println(String.valueOf(res[i]));
-            }
-        } catch (UnsatisfiedLinkError e) {
-            System.err.println("Native code library failed to load.\n" + e);
-            System.exit(1);
+        switch (number){
+            case "1":
+                System.out.println("Wyświetl choroby");
+                try {
+                    System.out.println("Choroby");
+                    Query query = new Query("consult('src/med.pl')");
+                    query.hasSolution();
+                    query = new Query("choroby(X)");
+
+                    Map < String, Term > [] res = query.allSolutions();
+                    for (int i = 0; i < res.length; i++) {
+                        System.out.println(String.valueOf(res[i]));
+                    }
+                } catch (UnsatisfiedLinkError e) {
+                    System.err.println("Native code library failed to load.\n" + e);
+                    System.exit(1);
+                }
+                break;
+
+            case "2":
+                System.out.println("DIAGNOZA");
+                try {
+                    System.out.println("Obrajwy tej choroby");
+                    Query query = new Query("consult('src/med.pl')");
+                    query.hasSolution();
+                    query = new Query("diagnoza(_,A,B,C,D)");
+
+                    Map < String, Term > [] res = query.allSolutions();
+                    for (int i = 0; i < res.length; i++) {
+                        System.out.println(String.valueOf(res[i]));
+                    }
+                } catch (UnsatisfiedLinkError e) {
+                    System.err.println("Native code library failed to load.\n" + e);
+                    System.exit(1);
+                }
+                break;
+
         }
+
+
+
+
     }
 }
--- Java+Prolog/src/med.pl
+++ Java+Prolog/src/med.pl
@@ -2,11 +2,19 @@ choroby(achadlazja).
 choroby(glejak_nerwu_wzrokowego).
 choroby(ebola).
 choroby(goraczka_okopowa).
+choroby(swiatlowstret).
+choroby(depresja).
+choroby(schizofrenia).
+choroby(anoreksja).
 
 choroba(achadlazja,zaburzenia_polykania,zgaga,przewlekly_kaszel, krztuszenie_sie).
 choroba(glejak_nerwu_wzrokowego,wytrzeszcz_galki_ocznej,zgaga,zgaga,uposledzenie_widzenia).
 choroba(ebola, bola_glowy, bol_miesni, bol_stawow, krwawa_biegunka).
 choroba(goraczka_okopowa, goraczka, bol_glowy, sztywnosckarku, swiatlowstret).
+choroba(swiatlowstret,zapalenie_spojowek, zapalenie_wnetrza_galki_ocznej, bezteczowkowosc, bezsoczewkowosc).
+choroba(depresja, zaburzenia_snu, spadek_libido, zmniejszenie_lub_utrata_apetytu, dobowe_wahanie_samopoczucia).
+choroba(schizofrenia, halucynacje, urojenia_ksobne, anhedonia, awolicja).
+choroba(anoreksja, obnizenie_nastroju, poczucie_braku_sensu_zycia, mysli_samobojcze, objawy_myslenia_obsesyjnego)
 
 diagnoza(X,A,B,C,D):-
             read(X),nl,
